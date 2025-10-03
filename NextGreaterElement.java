3f9d1ab37f0ab698bbaee3e85fbf774b6d533e12
Shivang <sraj98556@gmail.com>
2025-10-03 08:25:54 +0530

    File Updated

--- NextGreaterElement.java
+++ NextGreaterElement.java
@@ -1,4 +1,16 @@
 package DSA;
+import java.util.*;
 
 public class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums2.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<nums2.length; i++){
+            
+        }
+
+
+
+    }
 }
